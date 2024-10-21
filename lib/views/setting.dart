3b6457043a5909d0f5432c66d0eab4a3e940f557
mtFuji <55128509+Yuto-Fujioka@users.noreply.github.com>
2024-10-22 00:45:28 +0900

    [fix] vercelPageRoute

--- lib/views/setting.dart
+++ lib/views/setting.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:wakutori/widgets/setting_widget/app_bar.dart';
+import 'package:wakutori/widgets/setting_widget/body.dart';
 
 class Setting extends StatelessWidget {
   const Setting({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ListView();
+    return const Scaffold(
+      appBar: SettingAppBar(),
+      body: SettingBody(),
+    );
   }
 }
--- lib/widgets/setting_widget/app_bar.dart
+++ lib/widgets/setting_widget/app_bar.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 
-class SettingAppBar extends StatelessWidget {
+class SettingAppBar extends StatelessWidget implements PreferredSizeWidget {
   const SettingAppBar({Key? key}) : super(key: key);
 
   @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+
+  @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return AppBar(
+      automaticallyImplyLeading: false,
+      elevation: 5,
+    );
   }
   
 }
