17e9ae44bdffe4cfedb7abfddcf911cb8c9d8dea
TongariAtamago <takuyamao@outlook.jp>
2024-12-25 15:38:11 +0900

    [feat] Assign bands for days and times.

--- lib/services/setting_service.dart
+++ lib/services/setting_service.dart
@@ -1,10 +1,12 @@
+// import 'dart:math';
+
+// import 'package:flutter/material.dart';
 import 'dart:math';
 
-import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:wakutori/utils/half_mode.dart';
 import 'package:wakutori/utils/logger.dart';
-import 'dart:collection' as collection;
+// import 'dart:collection' as collection;
 
 import '../utils/contents_class.dart';
 
@@ -44,85 +46,123 @@ void setAllSchedulePreviousWeek() async {
 }
 
 void setAllSchedule(
-    int bandCount, String primaryWord, int howToSet, bool isRewritable) async {
+    final int bandCount,
+    final String primaryWord,
+    final int howToSet,
+    final bool isRewritable) async {
   final SupabaseClient supabase = Supabase.instance.client;
-  final List<Map<String, dynamic>> bands = await supabase.from('band').select();
 
-  const List<String> dates = ['火', '水', '木', '金', '土', '日', '月'];
+  const List<String> dates = ['水', '木', '金', '土', '日', '月', '火'];
   const List<String> times = ['1限', '2限', '昼休み', '3限', '4限', '5限', '6限', '7限'];
 
-  //入力のあるバンドとbandCountのチェック用
-  final Map<String, int> bandNameCheck = {};
+  final List<Map<String, dynamic>> bands = await supabase.from('band').select();
+  final Map<String, int> pendingTimeOfBands = {
+    for (final Map<String, dynamic> band in bands) band['band']: bandCount
+  };
+  final Map<String, String> completedSchedule = {
+    for (final String date in dates)
+      for (final String time in times) '$date曜日 $time': ''
+  };
+  final Map<String, Map<String, int>> dayBands = {
+    for (final String date in dates)
+      for (final String time in times) '$date曜日 $time': {}
+  };
 
-  //最終リストの定義
-  final Map<String, Map<String, int>> completeList = {};
-  for (String date in dates) {
-    for (String time in times) {
-      completeList.addAll({
-        '$date曜日 $time': {}, //ここはなんとなく'':0で初期化してたのを一旦消したけど、必要なら復活させて！
-      });
-    }
-  }
-
-  //バンドごとのランキング
-  Map<String, Map<String, int>> rankedBands = {};
-  for (Map<String, dynamic> band in bands) {
+  final Map<String, Map<String, int>> rankedBands = <String, Map<String, int>>{};
+  for (final Map<String, dynamic> band in bands) {
     final String name = band['band'];
-    bandNameCheck.addAll({
-      name: 0,
-    });
-    rankedBands.addAll({name: {}});
-
+    rankedBands[name] = {};
     final List<Map<String, dynamic>> rowData =
-        await supabase.from('schedule').select().eq('band_name', name);
+    await supabase.from('schedule').select().eq('band_name', name);
     final List<ContentsClass> classedData =
-        rowData.map((element) => ContentsClass.fromJson(element)).toList();
+    rowData.map((final Map<String, dynamic> data) => ContentsClass.fromJson(data)).toList();
 
-    Map<String, int> bestTimeAsMap = {};
-    for (ContentsClass data in classedData) {
+    final Map<String, int> bestTimeAsMap = <String, int>{};
+    for (final ContentsClass data in classedData) {
       final String key = '${dates[data.weekday]}曜日 ${times[data.time]}';
-      bestTimeAsMap.update(key, (i) => ++i, ifAbsent: () => 1);
+      bestTimeAsMap[key] = (bestTimeAsMap[key] ?? 0) + 1;
+      pendingTimeOfBands[name] = min((pendingTimeOfBands[name] ?? 0) + 1, bandCount);
     }
 
-    //団体練、個人練、優先する単語の重み付け
-    rankedBands[name] = bestTimeAsMap.map((dateTime, density) {
-      if (name.contains('団体練')) {
-        return MapEntry(dateTime, density + 100);
-      } else if (name.contains('個人練')) {
-        return MapEntry(dateTime, density - 50);
-      } else if (primaryWord.isNotEmpty && name.contains(primaryWord)) {
-        return MapEntry(dateTime, density + 50);
-      } else {
-        return MapEntry(dateTime, density);
-      }
+    rankedBands[name] = bestTimeAsMap.map((final String dateTime, final int density) {
+      int weight = density;
+      if (name.contains('団体練')) weight += 100;
+      if (name.contains('個人練')) weight -= 50;
+      if (primaryWord.isNotEmpty && name.contains(primaryWord)) weight += 50;
+      return MapEntry(dateTime, weight);
     });
 
-    //rankedBands内の各dateTimeをキー、map<String, int>にrankedBandsのname、densityを代入したvalueを持つmapを作成
-
-    if (rankedBands[name]!.isEmpty) {
-      rankedBands.remove(name);
-    }else{
-      rankedBands[name]!.forEach((dateTime, density) {
-        if (!completeList.containsKey(dateTime)) {
-          completeList[dateTime] = {};
-        }
-        completeList[dateTime]![name] = density;
-      });
-    }
-
-    //rankedBands内の各バンドを重みで降順ソート
-    rankedBands.forEach((bandName, bandData) {
-      final List<MapEntry<String, int>> sortedData = bandData.entries.toList()..sort((a, b) => b.value.compareTo(a.value));
-      rankedBands[bandName] = Map.fromEntries(sortedData);
-    });
-
-    //completeList内の各dateTimeを重みで降順ソート
-    completeList.forEach((dateTime, bandData) {
-      final List<MapEntry<String, int>> sortedData = bandData.entries.toList()..sort((a, b) => b.value.compareTo(a.value));
-      completeList[dateTime] = Map.fromEntries(sortedData);
+    rankedBands[name]!.forEach((final String dateTime, final int density) {
+      dayBands[dateTime]![name] = density;
     });
   }
 
-  logger.i(rankedBands);
-  logger.i(completeList);
+  _distributeSchedule(dayBands, pendingTimeOfBands, completedSchedule);
+  logger.i(completedSchedule);
 }
+
+void _distributeSchedule(
+    final Map<String, Map<String, int>> dayBands,
+    final Map<String, int> pendingTimeOfBands,
+    final Map<String, String> completedSchedule) {
+  final Map<String, int> assignedCount = {
+    for (final String band in pendingTimeOfBands.keys) band: 0
+  };
+  final Map<String, List<String>> conflictMap = <String, List<String>>{};
+
+  final String firstDate = dayBands.keys.first;
+  if (dayBands[firstDate]!.isNotEmpty) {
+    final MapEntry<String, int> highestPriorityBand = dayBands[firstDate]!.entries
+        .reduce((final MapEntry<String, int> a, final MapEntry<String, int> b) => a.value >= b.value ? a : b);
+    completedSchedule[firstDate] = highestPriorityBand.key;
+    assignedCount[highestPriorityBand.key] = 1;
+  }
+
+  final List<MapEntry<String, int>> sortedBandsByRemaining =
+  pendingTimeOfBands.entries.toList()
+    ..sort((final MapEntry<String, int> a, final MapEntry<String, int> b) => a.value.compareTo(b.value));
+  for (final MapEntry<String, int> entry in sortedBandsByRemaining) {
+    final String bandName = entry.key;
+    if (entry.value > 0) {
+      for (final String date in dayBands.keys) {
+        if (completedSchedule[date]!.isEmpty && dayBands[date]!.containsKey(bandName)) {
+          completedSchedule[date] = bandName;
+          assignedCount[bandName] = (assignedCount[bandName] ?? 0) + 1;
+          break;
+        }
+      }
+    }
+  }
+
+  for (final String date in dayBands.keys.where((final String date) => completedSchedule[date]!.isNotEmpty)) {
+    final String band = completedSchedule[date]!;
+    if (assignedCount[band]! > pendingTimeOfBands[band]!) {
+      conflictMap.update(date, (final List<String> list) => [...list, band], ifAbsent: () => [band]);
+    }
+  }
+
+  for (final String date in conflictMap.keys) {
+    final List<String> conflictingBands = conflictMap[date]!;
+    conflictingBands.sort((final String a, final String b) => assignedCount[a]!.compareTo(assignedCount[b]!));
+    for (final String band in conflictingBands) {
+      if (assignedCount[band]! > pendingTimeOfBands[band]!) {
+        completedSchedule[date] = '';
+        assignedCount[band] = (assignedCount[band] ?? 1) - 1;
+        break;
+      }
+    }
+  }
+
+  for (final String band in pendingTimeOfBands.keys) {
+    if (assignedCount[band] == 0) {
+      for (final String date in dayBands.keys) {
+        if (completedSchedule[date]!.isEmpty && dayBands[date]!.containsKey(band)) {
+          completedSchedule[date] = band;
+          assignedCount[band] = (assignedCount[band] ?? 0) + 1;
+          break;
+        }
+      }
+    }
+  }
+}
+
